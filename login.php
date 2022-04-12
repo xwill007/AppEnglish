e5d8d786fac1dbb64b19d17236f9fff90d4c9f0f
wilbervargas <wilber.vargas@udea.edu.co>
2022-04-11 21:36:16 -0500

    ingreso protegido consola admin

--- login.php
+++ login.php
@@ -12,7 +12,13 @@ if (mysqli_num_rows($Respuesta)>0){
 	$nombre = $Result->fetch_array()[0]??'';
 	$_SESSION['name'] = $nombre; 
 	$_SESSION['auth'] = true;
-	header("Location: inicio.php");
+	if($_SESSION['user'] == "admin@admin.com"){
+		$_SESSION['admin'] = true;
+		header("Location: adminControl.php");	
+	}else{
+		$_SESSION['admin'] = false;
+		header("Location: inicio.php");
+	}
 }else{
 	print("Error<br>");
 	header("Location: index.php?error=1");
